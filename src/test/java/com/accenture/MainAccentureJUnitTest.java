dbd12e846b8473baaa10a8d05d22e2706db919f1
yeison34k <55606317+yeison34k@users.noreply.github.com>
2019-09-22 15:20:40 -0500

    cambios generales del codigo

--- src/test/java/com/accenture/MainAccentureJUnitTest.java
+++ src/test/java/com/accenture/MainAccentureJUnitTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -29,74 +30,95 @@ import org.springframework.web.context.WebApplicationContext;
 /**
  *
  * @author Yesion Aristizabal
- * Pruebas Main 
+ * Pruebas generales de la aplicaciòn
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MainAccentureJUnitTest {
     
-//    public MainAccentureJUnitTest() {
-//    }
-//    
-//    @Autowired
-//    private WebApplicationContext wac;
-//    private MockMvc mockMvc;
-//    
-//    @Before
-//    public void setup() throws Exception {
-//        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-//    }
-//    
-//    /**
-//     * Send GET Request with Path Variable
-//     *
-//     * @throws Exception
-//     */
-//    @Test
-//    public void givenSedeURIWithPathVariable_whenMockMVC_thenResponseOK() throws Exception {
-//        this.mockMvc
-//                .perform(get("/usuario/listar"))
-//                .andDo(print()).andExpect(status().isOk())
-//                .andExpect(content().contentType("application/json;charset=UTF-8"));
-//    }
-//
-//    /**
-//     * Enviar POST 
-//     * @throws Exception
-//     */
-//    @Test
-//    public void validoIngresoDatosAlGuardarUsuario() throws Exception {
-//        String nombre = "NombrePruebas" + new Date().getTime();
-//        String apellido = "ApellidoPruebas" + new Date().getTime();
-//        
-//        UsuarioResponse usuario = new UsuarioResponse();
-//        usuario.setNombre(nombre);
-//        usuario.setActivo('s');
-//        usuario.setApellidos(apellido);
-//        usuario.setCorreo(nombre.toLowerCase()+ "@gmail.com");
-//        usuario.setTelefono("+571564246");
-//        usuario.setSexo('m');
-//        
-//        //pruebo servicio rest
-//        this.mockMvc.perform(post("/usuario/guardarOModificar.json")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(asJsonString(usuario)))
-//                .andDo(print()).andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-//                .andExpect(jsonPath("$.nombre").value(nombre))
-//                .andExpect(jsonPath("$.apellido").value(apellido));
-//    }
-//
-//    /**
-//     * converts a Java object into JSON representation
-//     * @param obj
-//     * @return 
-//     */
-//    public static String asJsonString(final Object obj) {
-//        try {
-//            return new ObjectMapper().writeValueAsString(obj);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    public MainAccentureJUnitTest() {
+    }
+    
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setup() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+    
+    /**
+     * @Author yeison aristizabal
+     * Enviar POST 
+     * @throws Exception
+     * 
+     * https://blog.marcnuri.com/mockmvc-introduccion-a-spring-mvc-testing/
+     */
+    @Test
+    public void validoIngresoDatosAlGuardarUsuario() throws Exception {
+        String nombre = "NombrePruebas" + new Date().getTime();
+        String apellido = "ApellidoPruebas" + new Date().getTime();
+        
+        UsuarioResponse usuario = new UsuarioResponse();
+        usuario.setId(90l);
+        usuario.setNombre(nombre);
+        usuario.setActivo('s');
+        usuario.setApellidos(apellido);
+        usuario.setCorreo(nombre.toLowerCase()+ "@gmail.com");
+        usuario.setTelefono("+571564246");
+        usuario.setSexo('m');
+        
+        //pruebo servicio rest
+        this.mockMvc.perform(post("/usuario/guardarOModificar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(usuario)))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.nombre").value(nombre));
+    }
+    
+    
+    /**
+     * @Author yeison aristizabal
+     * Send GET Request with Path Variable
+     *
+     * @throws Exception
+     */
+    @Test
+    public void givenSedeURIWithPathVariable_whenMockMVC_thenResponseOK() throws Exception {
+        ResultActions result = this.mockMvc
+                .perform(get("/usuario/listar"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+        
+        result.andExpect(status().isOk());
+    }
+    
+    /**
+     * @Author yeison aristizabal
+     * @throws Exception 
+     */
+    @Test
+    public void getUsuarioById() throws Exception {
+        ResultActions result = this.mockMvc
+            .perform(get("/usuario/get/23" ))
+            .andDo(print()).andExpect(status().isOk())
+            .andExpect(content().contentType("application/json;charset=UTF-8"));
+        
+        result.andExpect(status().isOk());
+    }
+
+    /**
+     * converts a Java object into JSON representation
+     * @param obj
+     * @return 
+     */
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
